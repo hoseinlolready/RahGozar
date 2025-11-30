12d309276ad661c76c32eecb8c989a804764fdce
HOSEINLOL <184541064+hoseinlolready@users.noreply.github.com>
2025-11-30 15:21:51 +0330

    Create uninstaller script for RahGozar
    Adds a script to uninstall the RahGozar application, removing its files and disabling the service.

---
+++ scripts/uninstaller.sh
@@ -0,0 +1,16 @@
+echo [System] : Uninstalling RahGozar
+echo [RahGozar] : Naaaaaaaa
+sleep 2
+sudo rm -fr /opt/Rahgozar/
+sudo rm /usr/bin/Rahgozar
+sleep 1
+echo [RahGozar] : ...
+sudo rm /etc/systemd/system/Rahgozar.service
+sudo systemctl disable Rahgozar.service
+sleep 1
+echo [System] : RahGozar uninstalled successfully
+sleep 2
+Echo [Rahgozar Creator] : Narahat shodam.
+sleep 2
+Echo [Rahgozar Creator] : Bye.
+sleep 2
